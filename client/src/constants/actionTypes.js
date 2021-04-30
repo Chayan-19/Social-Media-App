4449d430d85f648ea2363da70ca88f57e4f9a570
Chayan <55481820+Chayan-19@users.noreply.github.com>
2021-04-30 18:30:54 +0530

    Add files via upload

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,7 @@
+export const CREATE = "CREATE",
+  DELETE = "DELETE",
+  UPDATE = "UPDATE",
+  FETCH_ALL = "FETCH_ALL",
+  LIKE = "LIKE",
+  AUTH = "AUTH",
+  LOGOUT = "LOGOUT";
---
+++ client/src/reducers/posts.js
@@ -0,0 +1,27 @@
+import {
+  FETCH_ALL,
+  UPDATE,
+  DELETE,
+  LIKE,
+  CREATE,
+} from "../constants/actionTypes";
+
+const reducer = (posts = [], action) => {
+  switch (action.type) {
+    case DELETE:
+      return posts.filter(post => post._id !== action.payload);
+    case UPDATE:
+    case LIKE:
+      return posts.map(post =>
+        post._id === action.payload._id ? action.payload : post
+      );
+    case FETCH_ALL:
+      return action.payload;
+    case CREATE:
+      return [...posts, action.payload];
+    default:
+      return posts;
+  }
+};
+
+export default reducer;
